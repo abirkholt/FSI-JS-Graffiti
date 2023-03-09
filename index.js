d61f3ab1a50498f2bf8e1005369d3c2d00db889f
abirkholt <abirkholt@gmail.com>
2023-03-08 19:34:34 -0800

    lessonplan

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Select some elements...
+// Select some elements....
+const header = document.querySelector('#page-header')
+header.style.textAlign ='left';
+
+const dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++) {
+    dogNames[i].style.textAlign = 'left';
+}
+
+const dogImages = document.querySelectorAll('.dog-image')
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.rotate = '180deg';
+    dogImages[i].style.borderRadius = '50px';
+}
+
+const footer = document.querySelector('.footer')
+footer.style.color = 'red';
+
